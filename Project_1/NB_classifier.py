bf01cbbe6ce41767c685678f7266b5d4d6075266
Eddy Pan <e.pan2@hotmail.com>
2018-03-19 22:08:33 +1100

    Added new class 'SupervisedModel' to build the Naive Bayes probabilistic model from the dataset

--- Project_1/NB_classifier.py
+++ Project_1/NB_classifier.py
@@ -3,13 +3,14 @@
 # Naive Bayes classifier
 
 import numpy as np
+from collections import defaultdict
 
 class DataSet:
 
     def __init__(self, filename):
         """
         Reads in the csv file into an appropriate data structure
-        :param filename:
+        :param filename: Name of the .csv file
         """
         # Variables to store metadata about the table structure
         self.num_rows = 0
@@ -22,7 +23,7 @@ class DataSet:
             self.num_cols = len(row)
             # Add row to table and increment row count
             self.table.append(row)
-            self.num_rows = self.num_rows + 1
+            self.num_rows += 1
         file.close()
 
     def get_num_rows(self):
@@ -50,5 +51,25 @@ class DataSet:
         return result
 
 
-breast = DataSet('breast-cancer.csv')
+class SupervisedModel:
 
+    def __init__(self, dataset):
+        """
+
+        :param dataset: A DataSet object containing the .csv file to calculate
+                        probabilities from
+        """
+        # Variables to store both the prior and posterior probabilities
+        self.priors = defaultdict(int)
+        self.posteriors = {}
+
+        # Add class counts to default dict
+        for row in dataset.table:
+            self.priors[row[-1]] += 1
+
+    def get_priors(self):
+        return self.priors
+
+
+breast = DataSet('breast-cancer.csv')
+a = SupervisedModel(breast)
